e86110cb18166fef6c9d9ef5352902c1168bad2e
Eros Coelho <eros.limacoelho@student.griffith.ie>
2024-07-24 17:28:13 +0100

    made events page and started converting "overview" into "quiz"

---
+++ JavaScript/quiz.js
@@ -0,0 +1,18 @@
+function submitQuiz() {
+    let quizForm = document.getElementById('quizForm');
+    let formData = new FormData(quizForm);
+    let correctAnswers = {
+        question1: "Alfred Hitchcock",
+        question2: "Camp Crystal Lake",
+        question3: "Pazuzu"
+    };
+    
+    let score = 0;
+    for (let [key, value] of formData.entries()) {
+        if (value === correctAnswers[key]) {
+            score++;
+        }
+    }
+    
+    alert("Your score: " + score + "/3");
+}
